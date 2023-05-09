b6a27e11ad9c9b90c0ca51db58b2ae8157531817
Dan Mahoney <dan.mahoney@tatari.tv>
2023-05-09 16:02:37 -0400

    plugin setup

---
+++ lua/user/copilot.lua
@@ -0,0 +1,27 @@
+local status_ok, copilot = pcall(require, "copilot")
+
+copilot.setup = function()
+    vim.g.copilot_filetypes = {
+        ["*"] = false,
+        -- ["markdown"] = true,
+        bash = true,
+        c = true,
+        cpp = true,
+        go = true,
+        html = true,
+        javascript = true,
+        javascriptreact = true,
+        lua = true,
+        python = true,
+        rust = true,
+        sh = true,
+        terraform = true,
+        typescript = true,
+        typescriptreact = true,
+    }
+    vim.cmd([[
+      imap <silent><script><expr> <C-j> copilot#Accept("\<CR>")
+      let g:copilot_no_tab_map = v:true
+    ]])
+end
+

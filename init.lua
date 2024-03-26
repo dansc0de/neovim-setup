b93efff9bba80992dcd6a86de10778ac8d113f47
Dan Mahoney <dan.mahoney@tatari.tv>
2024-03-26 11:43:41 -0400

    switch to lazy vim

---
+++ init.lua
@@ -0,0 +1,26 @@
+-- load lazy.nvim package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- vim options
+require("vim-options")
+
+-- lazy
+require("lazy").setup("plugins")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  command = "set nornu nonu | Neotree toggle",
+})
+vim.api.nvim_create_autocmd("BufEnter", {
+  command = "set rnu nu",
+})

6fdf0eb5f012495e5dbd25011f23e755b51182a7
dansc0de <dan.mahoney@protonmail.com>
2024-05-14 18:05:34 -0400

    fixup toggle term

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,8 +2,8 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.5',
-    dependencies = { 
-      'nvim-lua/plenary.nvim' 
+    dependencies = {
+      'nvim-lua/plenary.nvim'
     },
     config = function()
       local builtin = require('telescope.builtin')
--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -1,29 +1,24 @@
 return {
-  'akinsho/toggleterm.nvim',
-  version = '*',
-  config = function()
-    require("toggleterm").setup({
-      size = 20,
-      open_mapping = [[<c-\>]],
-      hide_numbers = true,
-      shade_filetypes = {},
-      shade_terminals = true,
-      shading_factor = 2,
-      start_in_insert = true,
-      insert_mappings = true,
-      persist_size = true,
+  {
+    "akinsho/toggleterm.nvim",
+    config = true,
+    cmd = "ToggleTerm",
+    keys = {
+      {
+        "<F4>",
+        "<cmd>ToggleTerm<cr>",
+        desc = "Toggle floating terminal"
+      }
+    },
+    opts = {
+      open_mapping = [[F4]],
       direction = "float",
+      shade_filetypes = {},
+      hide_numbers = true,
+      insert_mappings = true,
+      terminal_mappings = true,
+      start_in_insert = true,
       close_on_exit = true,
-      shell = vim.o.shell,
-      float_opts = {
-        border = "curved",
-        winblend = 0,
-        highlights = {
-          border = "Normal",
-          background = "Normal",
-        },
-      },
-    })
-
-  end
+    }
+  }
 }

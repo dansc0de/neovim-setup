727d0e273b4b415fec5b0bc48e038018a05ca472
dansc0de <dan.mahoney@protonmail.com>
2024-05-15 08:56:31 -0400

    remove neotree default L on vim enter

--- init.lua
+++ init.lua
@@ -18,9 +18,9 @@ require("vim-options")
 -- lazy
 require("lazy").setup("plugins")
 
-vim.api.nvim_create_autocmd("VimEnter", {
-  command = "set nornu nonu | Neotree toggle",
-})
+--vim.api.nvim_create_autocmd("VimEnter", {
+--  command = "set nornu nonu | Neotree toggle",
+--})
 vim.api.nvim_create_autocmd("BufEnter", {
   command = "set rnu nu",
 })

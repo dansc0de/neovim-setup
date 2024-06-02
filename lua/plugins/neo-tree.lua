c604812983a0a248d19e1723a7e3cb9632839132
dansc0de <dan.mahoney@protonmail.com>
2024-06-02 11:37:42 -0400

    update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,5 +8,21 @@ return {
   },
   config = function()
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-  end
+  end,
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+           -- '.git',
+           -- '.DS_Store',
+           -- 'thumbs.db',
+         },
+        never_show = {},
+        },
+    }
+  }
 }
